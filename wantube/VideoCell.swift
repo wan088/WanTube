d51084e8e608ff34463742e02fffa65d820e5e05
Yongwan <wanrage@naver.com>
2019-07-27 18:11:42 +0900

    ViewController -> HomeController 변경 & VideoCell 클래스를 별도의 파일로 분리

--- wantube/VideoCell.swift
+++ wantube/VideoCell.swift
@@ -1,43 +1,14 @@
 //
-//  ViewController.swift
+//  VideoCell.swift
 //  wantube
 //
-//  Created by Yongwan on 26/07/2019.
+//  Created by Yongwan on 27/07/2019.
 //  Copyright © 2019 Yongwan. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class HomeController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        
-        let titleView = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width - 32, height: self.view.frame.height))
-        titleView.textAlignment = .left
-        titleView.textColor = .white
-        titleView.font = .boldSystemFont(ofSize: 20)
-        titleView.text = "WanTube"
-        self.navigationItem.titleView = titleView
-        
-        
-        self.tableView.register(VideoCell.self, forCellReuseIdentifier: "cellId")
-        self.navigationController?.navigationBar.barTintColor = .red
-        self.navigationController?.navigationBar.isTranslucent = true
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "cellId")!
-        return cell
-    }
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 300
-    }
-}
 class VideoCell: UITableViewCell{
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
