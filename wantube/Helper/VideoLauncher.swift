f9aebbf72e1ef3ff6d26c78f52ff3e0a09e5925e
Yongwan <wanrage@naver.com>
2019-08-27 23:21:46 +0900

    add ControllConatainerView & Activiti Indicater

--- wantube/Helper/VideoLauncher.swift
+++ wantube/Helper/VideoLauncher.swift
@@ -10,8 +10,32 @@ import Foundation
 import UIKit
 import AVKit
 class VideoPlayerView: UIView{
+    let controlsContainerView: UIView = {
+        let ccv = UIView();
+        ccv.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        return ccv
+    }()
+    let activityIndicator :UIActivityIndicatorView = {
+        let aiv = UIActivityIndicatorView()
+        aiv.translatesAutoresizingMaskIntoConstraints = false
+        aiv.startAnimating()
+        return aiv
+    }()
     override init(frame: CGRect) {
         super.init(frame: frame)
+        self.setUpPlayerView()
+
+        self.controlsContainerView.frame = frame
+        self.addSubview(controlsContainerView)
+        
+        self.controlsContainerView.addSubview(self.activityIndicator)
+        
+        self.activityIndicator.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        self.activityIndicator.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        
+        
+    }
+    func setUpPlayerView(){
         self.backgroundColor = .black
         
         let urlString = "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726"
@@ -21,8 +45,13 @@ class VideoPlayerView: UIView{
             playerlayer.frame = self.frame
             self.layer.addSublayer(playerlayer)
             player.play()
+            player.addObserver(self, forKeyPath: "currentTime.loadedTimeRanges", options: .new, context: nil)
         }
-        
+    }
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "currentTime.loadedTimeRanges" {
+            print("받음")
+        }
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
